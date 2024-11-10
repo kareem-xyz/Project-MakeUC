eb36a98852a82700554c2add469e2099f71320b6
kareem-xyz <kareemrashad.sd@gmail.com>
2024-11-10 10:12:53 -0500

    Final Demo 1. Works from 2 Lines.

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from flask import Flask, render_template
-from helpers import * # import all helper function from helper.py
-
+from flask import Flask, jsonify, render_template
+from serial import Serial
 # Configure Application
 app = Flask(__name__)
 # Set the sessiono and data storage for app, used per user
@@ -9,18 +8,39 @@ app.config['SESSION_TYPE'] = 'filesystem' # use filesystem storage (server stora
 app.config['SESSION_PERMANENT'] = False
 app.config["TEMPLATES_AUTO_RELOAD"] = True # Ensure templates are auto-reloaded, saves times 
 
+arduino_1 = Serial(port='COM9', baudrate=9600, timeout=0)
+arduino_2 = Serial(port='COM11', baudrate=9600, timeout=0)
+
+
 # Load index (Homepage)
 @app.route('/')
 def index():
-    close_serial_connection()
-    # sensor_data = read_from_arduino().split(',')
-    
-    # return f"Sensor Value: {sensor_data}" if sensor_data else "No data received"
+    # close_serial_connection()
     sensor_data = [
-        {'number':0,'name':'Cairo Right', 'humidity':read_from_arduino()},
-        {'number':1,'name':'Cairo Left', 'humidity':read_from_arduino()}
-        ]
+    {'number':0,'name':'Cairo Right', 'humidity': read_from_arduino(arduino_1)},
+    {'number':1,'name':'Cairo Left', 'humidity': read_from_arduino(arduino_2)}
+    ]
+    # return f"Sensor Value: {sensor_data}" if sensor_data else "No data received"
     return render_template('home.html', sensor_data=sensor_data) # Change number to the actual read value
+
+# def close_serial_connection():
+#     if arduino.is_open:
+#         arduino.close()
+        
+def read_from_arduino(arduino_no):
+    if arduino_no.in_waiting > 0:
+        # CURRENTLY THIS POSTS ONLY ONE LINE. NEED TO SEE HOW TO ADD MULTIPLE VALUES
+        data = arduino_no.readline().decode('utf-8').strip() # Read a line and decode it ()
+        return data
+    return None
+
+@app.route('/refresh_values_')
+def refresh_values():
+    sensor_data = [
+    {'number':0,'name':'Cairo Right', 'humidity': read_from_arduino(arduino_1)},
+    {'number':1,'name':'Cairo Left', 'humidity': read_from_arduino(arduino_2)}
+    ]
+    return jsonify(sensor_data)
 
 # Run App
 if __name__ == '__main__':
